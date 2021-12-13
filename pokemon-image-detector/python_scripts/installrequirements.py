c1c3bd5d53f21016738ffc814a515541b6ed4e0c
Gavin Francis <gavinfrancis1312@aggiemail.usu.edu>
2021-12-12 22:19:04 -0700

    Revert "CS-5600-Final"
    This reverts commit 5a336e6b6c468d2d919969f668683883f0afc176.

--- pokemon-image-detector/python_scripts/installrequirements.py
+++ pokemon-image-detector/python_scripts/installrequirements.py
@@ -3,7 +3,8 @@ import subprocess
 import sys
 import shutil
 
-os.chdir('../')
+# cd C:\Users\gavin\Desktop\School\Current-Courses\CS-5600\Homework\CS5600_6600_F21_Project02
+
 cwd = os.getcwd()
 
 operator_sign = ''
@@ -12,18 +13,27 @@ if '/' in cwd:
 else:
     operator_sign ='\\'
 
-cwd = os.getcwd()
 
 print("\nInstalling YOLOv5 and Requirements...\n")
 
-subprocess.check_call(["git", "clone", "https://github.com/ultralytics/yolov5"])
-
+subprocess.run('git clone https://github.com/ultralytics/yolov5')
 cwd = cwd + operator_sign + 'yolov5'
 os.chdir(cwd)
+print("I am here: " , os.getcwd())
+subprocess.run('pip install -r requirements.txt')
 
-print(".\n.\n.")
+#os.mkdir('datasets')
+os.mkdir('weights')
 
-process2 = subprocess.Popen(["pip", "install" , "-r" , "requirements.txt"], stdout=subprocess.PIPE)
-output2 = process2.communicate()[0]
+#datasets_path = cwd + operator_sign + 'datasets'
+weights_path = cwd + operator_sign + 'weights'
 
-print("\nInstalling YOLOv5 and Requirements are COMPLETE!!...\n")
+os.chdir('../')
+cwd = os.getcwd() + operator_sign + 'trained_weights'
+os.chdir(cwd)
+
+weight_file = os.getcwd() + operator_sign + 'best.pt'
+
+shutil.copyfile(weight_file, weights_path)
+
+print("I am here: " , os.getcwd())
